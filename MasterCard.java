3c57969832fdc5a53f0be13cecafc8cabec59046
David Dominguez <dominguezdavid396@gmail.com>
2023-11-03 08:48:36 -0600

    d

--- MasterCard.java
+++ MasterCard.java
@@ -1,12 +1,18 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 // Si el primer dígito de la tarjeta es un 5 entonces es una tarjeta Mastercard
 
 public class MasterCard extends Transaction {
 
-    private String codeHashMD5;
     private long cifEmperador;
+
+    private StringBuilder hashMD5;
+    public StringBuilder getHashMD5() {
+        return hashMD5;
+    }
 
     public MasterCard(String user, int id, String publicKey, double amount, long cardNumber, int expirationDate, int cvv, int cuotas) {
         super(user, id, publicKey, amount, cuotas, cardNumber, expirationDate, cvv);
