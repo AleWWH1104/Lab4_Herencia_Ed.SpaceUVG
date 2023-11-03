7dfd21a6bb5e51b029dc96b729f9a140dbabcdeb
David Dominguez <dominguezdavid396@gmail.com>
2023-11-03 08:34:45 -0600

    implementacion de generador de hash md5 en clase MasterCard

--- MasterCard.java
+++ MasterCard.java
@@ -1,3 +1,6 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 // Si el primer dígito de la tarjeta es un 5 entonces es una tarjeta Mastercard
 
 public class MasterCard extends Transaction {
@@ -11,6 +14,23 @@ public class MasterCard extends Transaction {
 
     public long getCifEmperador() {
         return cifradoEmperador.cifrar(this.getCardNumber(), 3);
+    }
+
+    private StringBuilder generateHashMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = md.digest(input.getBytes());
+
+            StringBuilder hashString = new StringBuilder();
+            for (byte b : hashBytes) {
+                hashString.append(String.format("%02x", b));
+            }
+
+            return hashString;
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Algoritmo MD5 no compatible.");
+            return new StringBuilder();
+        }
     }
 
 }
