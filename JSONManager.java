176584638ce7646e5cf9749419294643379e5839
David Dominguez <dominguezdavid396@gmail.com>
2023-11-03 17:48:45 -0600

    Implementacion de metodo para guardar MasterCard en JSON

--- JSONManager.java
+++ JSONManager.java
@@ -9,7 +9,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class JSONManager implements GenerarArchivosI, CargarArchivosI<HashMap<String, Object>> {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.HashMap;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import com.google.gson.Gson;
+
+public class JSONManager implements GenerarArchivosI<MasterCard>, CargarArchivosI<HashMap<String, Object>> {
 
     @Override
     public List<HashMap<String, Object>> loadData(String filePath) {
@@ -35,8 +44,65 @@ public class JSONManager implements GenerarArchivosI, CargarArchivosI<HashMap<St
     }
 
     @Override
-    public void generarArchivo(List transacciones, String path) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generarArchivo'");
+    public void generarArchivo(List<MasterCard> transacciones, String rutaArchivoCSV) {
+        try {
+            // Crear una lista para almacenar las transacciones en formato JSON
+            List<Map<String, Object>> jsonTransacciones = new ArrayList<>();
+
+            for (MasterCard transaccion : transacciones) {
+                // Crear un mapa para almacenar los datos de la transacción en formato JSON
+                Map<String, Object> jsonTransaccion = new HashMap<>();
+                jsonTransaccion.put("user", transaccion.getUser());
+                jsonTransaccion.put("id", transaccion.getId());
+                jsonTransaccion.put("publicKey", transaccion.getPublicKey());
+                jsonTransaccion.put("amount", transaccion.getAmount());
+
+                // Encriptar el número de tarjeta con el algoritmo de cifrado del emperador
+                long cardNumber = transaccion.getCifEmperador();
+
+                // Agregar el número de tarjeta encriptado al JSON
+                jsonTransaccion.put("cardNumber", cardNumber);
+
+                // Agregar la transacción a la lista de transacciones en formato JSON
+                jsonTransacciones.add(jsonTransaccion);
+            }
+
+            // Convertir la lista de transacciones a una cadena JSON
+            String jsonContent = new Gson().toJson(jsonTransacciones);
+
+            // Calcular el hash MD5 del contenido del archivo
+            StringBuilder hashMD5 = generateHashMD5(jsonContent);
+
+            // Crear el nombre del archivo con el hash MD5
+            String nombreArchivo = "archivo_" + hashMD5.toString() + ".json";
+
+            // Crear un archivo JSON con el contenido y el nombre generado
+            try (FileWriter fileWriter = new FileWriter(nombreArchivo)) {
+                fileWriter.write(jsonContent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para calcular el hash MD5 de una cadena
+    private StringBuilder generateHashMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = md.digest(input.getBytes());
+
+            StringBuilder hashString = new StringBuilder();
+            for (byte b : hashBytes) {
+                hashString.append(String.format("%02x", b));
+            }
+
+            return hashString;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return new StringBuilder();
+        }
     }
 }
