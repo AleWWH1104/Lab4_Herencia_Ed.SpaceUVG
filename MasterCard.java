ffb06d1a9862e79cbba81f816daf9a1067b648c3
David Dominguez <dominguezdavid396@gmail.com>
2023-11-02 21:19:44 -0600

    Creacion de clase MasterCard

--- MasterCard.java
+++ MasterCard.java
@@ -1,12 +1,21 @@
+import java.util.List;
+import java.util.Map;
+
+// Si el primer dígito de la tarjeta es un 5 entonces es una tarjeta Mastercard
+
 public class MasterCard implements Pasarela_Pago {
 
     @Override
-    public void procesar_pago() {
-        }
+    public void procesar_pago(List<Map<String, Object>> transactions) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'procesar_pago'");
+    }
 
     @Override
-    public void cambiar_pasarela() {
-      }
+    public void cambiar_pasarela(Pasarela_Pago nuevaPasarela) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cambiar_pasarela'");
+    }
     
 }
 
